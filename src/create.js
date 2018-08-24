39236300ff4cd0d112baecbb4ebd2b90f66471cf
Richard G <richard.glenski@gmail.com>
2018-08-24 15:21:44 -0300

    logging

--- src/create.js
+++ src/create.js
@@ -14,7 +14,7 @@ export async function main(event, context, callback) {
       createdAt: Date.now()
     }
   };
-
+  console.log(params);
   try {
     await dynamoDbLib.call("put", params);
     callback(null, success(params.Item));
